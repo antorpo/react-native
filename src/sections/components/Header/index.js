ac830053e01bde6117bde925d0ef62b0b71b4454
antorpo <antonio.gonzalezr@udea.edu.co>
2020-07-07 23:11:28 -0500

    Creacion del primer componente, estilado usando StyleSheet de react-native.

---
+++ src/sections/components/Header/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {View, Text, Image, StyleSheet, SafeAreaView} from 'react-native';
+
+export const Header = ({children}) => {
+  const logoImg = require('../../../../assets/logo.png');
+
+  /*
+    Podriamos agregarle padding a la imagen directamente,
+    pero es mejor utilizar un div (View) para el contenido.
+   */
+  return (
+    <SafeAreaView>
+      <View style={styles.container}>
+        <View>
+          <Image source={logoImg} style={styles.logo} />
+        </View>
+        <View>{children}</View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+/* 
+  Una alternativa es darle estilos al contenedor de afuera 
+  y darle la propiedad flexDirection: 'row' y un
+  justifyContent: 'space-between'.
+*/
+const styles = StyleSheet.create({
+  logo: {
+    width: 80,
+    height: 26,
+    resizeMode: 'contain',
+  },
+  container: {
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+});
